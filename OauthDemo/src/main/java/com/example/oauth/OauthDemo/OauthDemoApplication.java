c99841ce70ffea6e170a9a91c6ea0e18f2714f88
TheRamC <ramchaudhariram@gmail.com>
2022-01-03 13:24:29 +0530

    Adding Oauth2 and JWT samples

---
+++ OauthDemo/src/main/java/com/example/oauth/OauthDemo/OauthDemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.oauth.OauthDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+
+@SpringBootApplication
+@EnableOAuth2Sso
+public class OauthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthDemoApplication.class, args);
+	}
+}
---
+++ OauthDemo/src/main/java/com/example/oauth/OauthDemo/controller/OauthController.java
@@ -0,0 +1,32 @@
+package com.example.oauth.OauthDemo.controller;
+
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+import java.util.Map;
+
+@RestController
+public class OauthController {
+
+    @GetMapping("/")
+    public String getUserDetails(Principal principal) {
+
+        OAuth2Authentication oAuth2Authentication = (OAuth2Authentication) principal;
+        boolean isAuthenticated = oAuth2Authentication.isAuthenticated();
+
+        Map<String, Object> details = (Map<String, Object>) oAuth2Authentication.getUserAuthentication().getDetails();
+        String username = (String) details.get("name");
+
+        return "Username: " + username
+                + "<br><br>" +
+                "Is User Authenticated? : " + isAuthenticated;
+    }
+
+    @GetMapping("/user")
+    public Principal showUser(Principal p) {
+        System.out.println(p.getClass().getName());
+        return p;
+    }
+}
